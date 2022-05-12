35869f34cfb5e60b823aed5845c67b31c8788a7d
Sami-Wafula <samiwafula3257@gmail.com>
2022-05-12 13:01:40 -0700

    Add beautiful_code.c Code that passes all Betty checks

---
+++ beautiful_code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - code that passes all the Betty checks
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("This code is beautiful, Betty agrees!\n");
+	return (0);
+}
